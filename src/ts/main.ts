3c479fc876a0c4d04c50c6d935824f17e1dd720d
bea0f <beatricefriberg@hotmail.com>
2024-03-15 12:22:14 +0100

    some changes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,46 @@
-import './../scss/style.scss'
+import "./../css/style.css";
 
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
+const hamburger = document.getElementById("hamburger") as HTMLDivElement;
+const navMenu = document.getElementById("nav-menu") as HTMLUListElement;
 
-hamburger?.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
-  navMenu?.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-/* document.querySelectorAll(".navLinks").forEach((n) =>
-    n.addEventListener("click", () => {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
+document.querySelectorAll(".navLinks").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+const projectContainer = document.getElementById("projectsCardContainer");
+const projects = document.getElementsByClassName("projectCards");
+
+Object.values(projects).forEach((project) => {
+  project.addEventListener("click", () => {
+    const myModal = document.createElement("div");
+    const modalContent = document.createElement("div");
+    const close = document.createElement("span");
+
+    myModal.classList.add("modal");
+    modalContent.classList.add("modal-content");
+    close.classList.add("close");
+    close.innerHTML = "&times;";
+
+    close.addEventListener("click", () => {
+      myModal.style.display = "none"
     })
-  ); */
+
+    myModal.appendChild(modalContent);
+    myModal.appendChild(close);
+    projectContainer?.appendChild(myModal);
+    
+  });
+});
+
+/* const projOne = document.getElementById("projectOne") as HTMLDivElement;
+
+projOne.addEventListener("click", () => {});
+//Skapa ett "card" för projekten?
+ */
